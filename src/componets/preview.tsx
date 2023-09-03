48a0c0149b4db90f97ce157dbc075c6d07a2b74a
Sebastian216 <35480323+Rynixx@users.noreply.github.com>
2023-09-03 17:31:52 +0200

    Mobile Update LP

--- src/componets/preview.tsx
+++ src/componets/preview.tsx
@@ -2,9 +2,9 @@ export default function Preview(props) {
   return (
     <>
       <div
-        class={`flex flex-col md:${props.flex} px-8 w-full items-center justify-center font-titillium mt-10 mb-10`}
+        class={`flex flex-col-reverse md:${props.flex} px-8 w-full items-center justify-evenly font-titillium mt-10 mb-10`}
       >
-        <div class="max-w-2xl flex flex-col -pr-20 md:pt-60 md:pb-60 md:p-12 md:m-10 m-2 justify-center md:space-x-2 space-y-4 md:w-2/3 h-auto bg-svg-bubble bg-contain bg-center bg-no-repeat">
+        <div class="max-w-2xl flex flex-col -pr-20 md:pt-60 md:pb-60 md:p-12 md:m-10 m-2 justify-center md:space-x-2 space-y-4 md:w-2/3 h-auto md:bg-svg-bubble bg-contain bg-center bg-no-repeat">
           <h3 class="text-4xl font-bold">{props.title}</h3>
           <p class="text-xl max-w-xl">{props.desc}</p>
           <span class="w-min pl-4 pr-4 bg-zinc-100 rounded-full">{props.labels}</span>
